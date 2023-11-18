1cd4ea8e700124e494344c17ab427c77469e718e
DamanPreet Singh <daman08@DamanPreets-MacBook-Air.local>
2023-11-18 19:59:08 +0530

    Firebase setup for ios, android & web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_app_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_app_flutter/responsive/responsive_layout.dart';
 import 'package:instagram_app_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_app_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensuring flutter widgets initialization
+  if (kIsWeb) {
+    // Web Firebase Setup
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyD6mGr7fNf8W6lLtRpgRvNDRv9tWbhHjeM',
+        appId: '1:907841466106:web:6694095a82721d7bb72343',
+        messagingSenderId: '907841466106',
+        projectId: 'instagram-clone08-9cf6a',
+        // added extra 'storageBucket'
+        storageBucket: 'instagram-clone08-9cf6a.appspot.com',
+      ),
+    ); // Initialising Firebase
+  } else {
+    // For mobile
+    await Firebase.initializeApp(); // Initialising Firebase
+  }
+
   runApp(const MyApp());
 }
 
